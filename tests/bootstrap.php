a00a5c51b6bad11df0573097fc2a0e43fd83f35e
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-03-09 17:55:28 +0200

    Travis config another fix attempt

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->add('Underscore\Test', __DIR__);
+//$loader = require __DIR__ . '/../vendor/autoload.php';
+//$loader->add('Underscore\Test', __DIR__);
+var_dump(get_declared_classes());
