c888e85c6f034a7a1dde1960782c8064a5ffdad3
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-03-12 23:13:10 +0200

    Added memoize() method

---
+++ lib/Underscore/Functions.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Underscore;
+
+/**
+ * Class Functions
+ * @package Underscore
+ */
+class Functions
+{
+    /**
+     * Creates a function that memoizes the result of the $payload
+     *
+     * @param callable $payload
+     *
+     * @return \Closure
+     */
+    public static function memoize($payload)
+    {
+        $function = function () use ($payload) {
+            static $cache = array();
+            $args = func_get_args();
+            $hash = md5(serialize($args));
+
+            if (!array_key_exists($hash, $cache)) {
+                $cache[$hash] = call_user_func_array($payload, $args);
+            }
+            return $cache[$hash];
+        };
+
+        return $function;
+    }
+}
---
+++ tests/Underscore/Test/FunctionsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Underscore\Test;
+
+use Underscore\Functions;
+
+/**
+ * Class FunctionsTest
+ * @package Underscore\Test
+ */
+class FunctionsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMemoize()
+    {
+        $counter = 0;
+
+        $payload = function () use (&$counter) {
+            ++$counter;
+        };
+
+        $memoized = Functions::memoize($payload);
+
+        $memoized();
+        $memoized();
+        $memoized();
+        $memoized();
+        $memoized();
+
+        $this->assertEquals(1, $counter);
+    }
+}
