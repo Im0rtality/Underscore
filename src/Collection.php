448d62acc45f2239b0a5919f27a555f5fab8519e
Woody Gilk <woody.gilk@gmail.com>
2015-07-06 23:56:35 -0500

    Convert Collection to extend from ArrayObject
    Makes Collection an extension of ArrayObject and converts all collections to
    output arrays. Object values are no longer preserved.
    
    __This is a breaking change!__
    
    Refs #49

--- src/Collection.php
+++ src/Collection.php
@@ -6,78 +6,29 @@ namespace Underscore;
  * Class Collection
  * @package Underscore
  */
-class Collection implements \ArrayAccess, \IteratorAggregate
+class Collection extends \ArrayObject
 {
-    /** @var  mixed */
-    protected $wrapped;
-
-    /**
-     * @param Object|array $object
-     */
-    public function __construct($object)
+    public function __construct($input)
     {
-        $this->wrapped = $object;
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @param int $offset
-     */
-    public function offsetExists($offset)
-    {
-        return $this->isObject() ? isset($this->wrapped->{$offset}) : isset($this->wrapped[$offset]);
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @param int $offset
-     */
-    public function offsetGet($offset)
-    {
-        return $this->isObject() ? $this->wrapped->{$offset} : $this->wrapped[$offset];
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @param int   $offset
-     * @param mixed $value
-     */
-    public function offsetSet($offset, $value)
-    {
-        if (null !== $offset) {
-            if ($this->isObject()) {
-                $this->wrapped->{$offset} = $value;
+        if (is_object($input)) {
+            if ($input instanceof \Traversable) {
+                $input = iterator_to_array($input);
             } else {
-                $this->wrapped[$offset] = $value;
-            }
-        } else {
-            if ($this->isObject()) {
-                $offset = 0;
-                while ($this->offsetExists($offset)) {
-                    $offset++;
-                }
-                $this->wrapped->{$offset} = $value;
-            } else {
-                $this->wrapped[] = $value;
+                $input = get_object_vars($input);
             }
         }
+        parent::__construct($input, static::ARRAY_AS_PROPS);
     }
 
     /**
-     * @inheritdoc
-     *
-     * @param int $offset
+     * @return \ArrayIterator
      */
-    public function offsetUnset($offset)
+    public function getIteratorReversed()
     {
-        if ($this->isObject()) {
-            unset($this->wrapped->{$offset});
-        } else {
-            unset($this->wrapped[$offset]);
-        }
+        $iterator   = $this->getIteratorClass();
+        $collection = array_reverse($this->getArrayCopy());
+
+        return new $iterator($collection);
     }
 
     /**
@@ -85,7 +36,7 @@ class Collection implements \ArrayAccess, \IteratorAggregate
      */
     public function value()
     {
-        return $this->wrapped;
+        return $this->toArray();
     }
 
     /**
@@ -93,55 +44,6 @@ class Collection implements \ArrayAccess, \IteratorAggregate
      */
     public function toArray()
     {
-        return $this->isObject() ? get_object_vars($this->wrapped) : $this->wrapped;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->toArray());
-    }
-
-    /**
-     * Returns reversed iterator. See getIterator() for docs.
-     */
-    public function getIteratorReversed()
-    {
-        return new \ArrayIterator(array_reverse($this->toArray(), true));
-    }
-
-    /**
-     * Gets the size of the collection by returning length for arrays or number of enumerable properties for objects.
-     * @return int
-     */
-    public function count()
-    {
-        return count($this->toArray());
-    }
-
-    public function __clone()
-    {
-        if ($this->isObject()) {
-            $this->wrapped = clone $this->wrapped;
-        } else {
-            $this->wrapped = array_map(
-                function ($item) {
-                    return $item;
-                },
-                $this->wrapped
-            );
-        }
-    }
-
-    /**
-     * Calls is_object on wrapped object
-     *
-     * @return bool
-     */
-    public function isObject()
-    {
-        return is_object($this->wrapped);
+        return $this->getArrayCopy();
     }
 }
